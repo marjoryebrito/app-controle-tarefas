f6f70e7b0d6b0ced20b9c229289d25dd8c109c6e
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-14 21:40:56 -0300

    Enviando e-mails.

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
 
 Route::get('/mensagem-teste', function(){
-    return new MensagemTesteMail();
+   // return new MensagemTesteMail();
+   Mail::to('emailteste@gmail.com')->send(new MensagemTesteMail());
+
+   return 'E-mail enviado com sucesso!';
+
 });
