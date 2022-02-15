9794f6ce03a4895c3e9516a18655fca3b165a260
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-15 18:49:37 -0300

    Customizando a view de verificação de e-mail.

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">Falta pouco agora! Precisamos apenas que você valide seu e-mail.</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                           Enviamos para você um e-mail de validação.
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    Antes de acessar os recursos da aplicação, por favor valide seu e-mail. <br><br>
+                    Se você não recebeu o e-mail,
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">clique aqui para enviarmos outro.</button>.
                     </form>
                 </div>
             </div>
