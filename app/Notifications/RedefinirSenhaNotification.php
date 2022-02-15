38fbc7e883c7555b114b0274c5578d09dd344e8d
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-15 17:17:36 -0300

    Configurando o envio de e-mails (Reset Password) - Parte 2

---
+++ app/Notifications/RedefinirSenhaNotification.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RedefinirSenhaNotification extends Notification
+{
+    use Queueable;
+
+    public $token;
+    public $email;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token, $email, $name)
+    {
+        $this->token = $token;
+        $this->email = $email;
+        $this->name = $name;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+
+        $url = 'http://localhost:8000/password/reset/'.$this->token.'?email='.$this->email;
+
+        $minutos = config('auth.passwords.'.config('auth.defaults.passwords').'.expire');
+
+        $saudacao = 'Olá, '.$this->name;
+
+        return (new MailMessage)
+            ->subject('Atualização de Senha')
+            ->greeting($saudacao)
+            ->line('Você está recebendo este e-mail porque recebemos uma solicitação de redefinição de senha para sua conta.')
+            ->action('Redefinir senha', $url)
+            ->line('O link acima expira em '.$minutos.' minutos.')
+            ->line('Se caso você não tenha solicitado a redefinição da senha, ignore este e-mail.')
+            ->salutation('Até breve!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
